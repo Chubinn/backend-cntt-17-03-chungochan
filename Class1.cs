cc25fede0ff6e06a5133c964ccca8cc684cec0c2
Chubinn <162661134+Chubinn@users.noreply.github.com>
2025-04-14 23:26:05 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class PhanSo
+{
+    public int Tu { get; set; }
+    public int Mau { get; set; }
+
+    public PhanSo()
+    {
+        Tu = 0;
+        Mau = 1;
+    }
+
+    public PhanSo(int tu, int mau)
+    {
+        Tu = tu;
+        Mau = mau == 0 ? 1 : mau;
+        RutGon();
+    }
+
+    public void NhapPhanSo()
+    {
+        Console.Write("Nhập tử số: ");
+        Tu = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhập mẫu số: ");
+        Mau = int.Parse(Console.ReadLine());
+
+        if (Mau == 0)
+        {
+            Console.WriteLine("Mẫu số không được bằng 0. Gán mẫu số = 1.");
+            Mau = 1;
+        }
+
+        RutGon();
+    }
+
+    public void RutGon()
+    {
+        int ucln = UCLN(Math.Abs(Tu), Math.Abs(Mau));
+        Tu /= ucln;
+        Mau /= ucln;
+
+        if (Mau < 0)
+        {
+            Tu = -Tu;
+            Mau = -Mau;
+        }
+    }
+
+    private int UCLN(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    public PhanSo Cong(PhanSo ps)
+    {
+        int tuMoi = this.Tu * ps.Mau + ps.Tu * this.Mau;
+        int mauMoi = this.Mau * ps.Mau;
+        return new PhanSo(tuMoi, mauMoi);
+    }
+
+    public override string ToString()
+    {
+        return $"{Tu}/{Mau}";
+    }
+}
+
---
+++ Class2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<PhanSo> danhSachPhanSo = new List<PhanSo>();
+
+        Console.Write("Nhập số lượng phân số: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Nhập phân số thứ {i + 1}:");
+            PhanSo ps = new PhanSo();
+            ps.NhapPhanSo();
+            danhSachPhanSo.Add(ps);
+        }
+
+        PhanSo tong = new PhanSo(0, 1);
+        foreach (var ps in danhSachPhanSo)
+        {
+            tong = tong.Cong(ps);
+        }
+
+        Console.WriteLine("Tổng các phân số là: " + tong);
+    }
+}
